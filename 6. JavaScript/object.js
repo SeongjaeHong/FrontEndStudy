a5869a63526cbe552f58d6a9a3ac636d6fb7c331
gtw04 <sjhong310@gmail.com>
2024-01-20 22:22:06 +0900

    js

--- 6. JavaScript/object.js
+++ 6. JavaScript/object.js
@@ -1,8 +1,38 @@
-function Person(name){
-    this.name = name;
-    this.description = 'My name is ' + this.name;
+function Calculate(){
+    this.read = function() {
+        this.n1 = +prompt();
+        this.n2 = +prompt();
+    };
+
+    this.sum = function() {
+        return this.n1 + this.n2;
+    };
 }
 
-var p1 = new Person('snowman');
-document.write(p1.name+"<br/>");
-document.write(p1.description+"<br/>");
+// let calculate = new Calculate();
+// calculate.read();
+// alert('sum = ' + calculate.sum());
+
+function Accumulator(num){
+    this.num = num;
+    this.input = [];
+    this.read = () => {
+        this.input.push(+prompt());
+    };
+    this.value = () => {
+        let num = this.num;
+        for(x in this.input){
+            num += this.input[x];
+        }
+        return num;
+    }
+}
+
+// acc = new Accumulator(4);
+// acc.read();
+// alert(acc.value());
+
+let something=null;
+let x = 5;
+something?.func(x++);  // nothing happens
+alert('x: ' + x);
