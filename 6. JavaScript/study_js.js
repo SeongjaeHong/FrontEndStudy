5e864e7afcc8ac5f1af3dc5cb3f5dd1a0aef5082
gtw04 <sjhong310@gmail.com>
2024-01-30 23:45:44 +0900

    study function

---
+++ 6. JavaScript/study_js.js
@@ -0,0 +1,79 @@
+/*
+function ask(question, ...handlers) {
+    let isYes = confirm(question);
+  
+    for(let handler of handlers) {
+      if (handler.length == 0) {
+        if (isYes) handler();
+      } else {
+        handler(isYes);
+      }
+    }
+  
+  }
+  
+  ask("질문 있으신가요?", () => alert('OK를 선택하셨습니다.'), result => alert(result));
+  */
+
+/*
+function makeCounter(){
+  function counter(){
+      return ++counter.count;
+  }
+  counter.count = 0;
+
+  return counter;
+}
+let counter = makeCounter();
+counter.count = 13;
+alert(counter())
+*/
+/*
+  let sayHi = function func(who){
+    if(who){
+        alert(`Hello ${who}`);
+    }
+    else{
+        func('Guest');
+    }
+  };
+
+let welcome = sayHi;
+sayHi = null;
+welcome();
+*/
+
+/*
+function makeCounter() {
+    function counter() {
+        return counter.count++;
+    }
+    counter.count = 0;
+    counter.set = function (value) {
+        counter.count = value;
+    };
+    counter.decrease = function () {
+        counter.count--;
+    };
+    return counter;
+}
+
+counter = makeCounter();
+counter.set(13);
+counter.decrease();
+alert(counter());
+*/
+
+function sum(num){
+    let num2 = num;
+
+    function f(n){
+        num2 += n;
+        return f;
+    }
+    f.toString = function(){
+        return num2;
+    };
+    return f;
+}
+alert(sum(5)(3));
