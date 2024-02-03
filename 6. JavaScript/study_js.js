48bc89f5e28f9e7ea1eb4dccf52f9faabf173e42
gtw04 <sjhong310@gmail.com>
2024-02-03 18:36:00 +0900

    study throttling

--- 6. JavaScript/study_js.js
+++ 6. JavaScript/study_js.js
@@ -97,10 +97,137 @@ function printNumbers(from, to) {
 printNumbers(5,6);
 
 */
-
+/*
 let i = 0;
 
 setTimeout(() => alert(i), 1);
 for(let j = 0; j < 10000; j++) {
   i++;
 }
+*/
+
+function work(a, b) {
+  alert(a + b);
+}
+
+function spy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, args);
+  }
+  wrapper.calls = [];
+  return wrapper;
+}
+
+// work = spy(work);
+// work(1, 2);
+// work(3, 4);
+// for (let args of work.calls) {
+//   alert('call:' + args.join());
+// }
+
+/*
+function f(x) {
+  alert(x);
+}
+
+function delay(func, time) {
+  function f() {
+    setTimeout(() => func.apply(this, arguments), time);
+  }
+  return f;
+  // return function(){setTimeout(()=>func.apply(this, arguments), time)};
+}
+
+let f1000 = delay(f, 3000);
+let f1500 = delay(f, 1500);
+
+f1000('test');
+f1500('test');
+*/
+
+/*
+function test(str){
+  console.log(str);
+}
+
+function debounce(func, delay){
+  let temp = '';
+  let timeId;
+
+  return function(){
+    temp = temp.concat(...arguments);
+    clearTimeout(timeId);
+    timeId = setTimeout(()=>func.call(this, temp), delay);
+  };
+}
+
+let f = debounce(test, 100);
+f('a');
+f('_hi');
+f('_hello');
+*/
+
+function f(a) {
+  console.log(a);
+}
+
+function throttle(func, ms) {
+  let saveArgs;
+  let throttledTime;
+  let throttlingTimeId;
+
+  function wrapper() {
+    if (throttlingTimeId == undefined) {
+      func.apply(this, arguments);
+      throttledTime = Date.now();
+      throttlingTimeId = 0;
+    }
+    else {
+      clearInterval(throttlingTimeId);
+      let timeInterval = Date.now() - throttledTime;
+      if (timeInterval > ms) {
+        func.apply(this, arguments);
+        throttledTime = Date.now();
+      }
+      else {
+        saveArgs = arguments;
+        delay = ms - timeInterval;
+        throttlingTimeId = setTimeout(() => func.apply(this, saveArgs), delay);
+      }
+    }
+  }
+
+  return wrapper;
+}
+
+function throttle2(func, ms) {
+  let savedArgs;
+  let isThrottled = false;
+
+  function wrapper() {
+    if (isThrottled) {
+      savedArgs = arguments;
+      return;
+    }
+
+    func.apply(this, arguments);
+    isThrottled = true;
+    
+    setTimeout(function(){
+      isThrottled = false;
+      if(savedArgs){
+        wrapper.apply(throttle2, savedArgs);
+        savedArgs = null;
+      }
+    }, ms);
+  }
+  return wrapper;
+}
+
+let ff = throttle2(f, 1000);
+ff(1);
+setTimeout(()=>ff.call(this, 2), 1000);  // this will be run in the last orde
+ff(3); // this will be ignored
+ff(4); // this will be ignored
+ff(5); // this will be run after 1000 ms
