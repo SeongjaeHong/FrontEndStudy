a6431e135d2ab6fc1171b1c7d50c823c94b124eb
gtw04 <sjhong310@gmail.com>
2024-02-04 18:54:12 +0900

    study prototype

---
+++ 6. JavaScript/prototype.js
@@ -0,0 +1,71 @@
+/*
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {    
+    __proto__: hamster,
+    stomach: []
+};
+
+let lazy = {
+    __proto__: hamster,
+    stomach: []
+};
+
+
+speedy.eat("apple");
+alert(speedy.stomach); // apple
+
+alert(lazy.stomach); // apple
+alert(hamster.stomach);
+*/
+
+/*
+Function.prototype.defer = function (ms) {
+    let x = this;
+    alert(`Function this:${this}`);
+    return function(){
+        alert(`Inner function this:${this}`);
+        setTimeout(()=>x.apply(this), ms)
+    };
+};
+let user = {
+    name: "John",
+    sayHi() {
+        alert(this.name);
+    }
+}
+
+user.sayHi = user.sayHi.defer(1000);
+user.sayHi();
+*/
+
+// let dictionary = Object.create(null, {
+//     toString: {
+//         value() {
+//             return Object.keys(this).join();
+//         }
+//     }
+// });
+let dictionary = Object.create(null);
+dictionary.toString = function(){
+    return Object.keys(this).join();
+};
+Object.defineProperty(dictionary, 'toString', {
+    enumerable: false,
+    configurable: false
+})
+
+dictionary.apple = 'Apple';
+dictionary.__proto__ = 'test';
+
+for(let key in dictionary){
+    alert(key);
+}
+
+alert(dictionary);
