5899ef6ceeb38cb54785fade3902dd7b8053eba5
gtw04 <sjhong310@gmail.com>
2024-02-12 19:53:38 +0900

    Async Study

--- 6. JavaScript/async.js
+++ 6. JavaScript/async.js
@@ -62,3 +62,43 @@ async function wait() {
 function f() {
     wait().then(result => alert(result));
 }
+
+function pseudoRandom(seed) {
+    let previous;
+    if (seed != undefined) { previous = seed; }
+    else { previous = Math.random(); }
+
+    return function () {
+        return previous = previous * 16807 % 2147483647;
+    }
+}
+
+let generator = pseudoRandom(1);
+
+// alert(generator()); // 16807
+// alert(generator()); // 282475249
+// alert(generator()); // 1622650073
+
+async function* fetchCommits(repo) {
+    let url = `https://api.github.com/repos/${repo}/commits`;
+    while (url) {
+        const response = await fetch(url, {
+            headers: { 'User-agent': 'My script' }
+        });
+        const body = await response.json();
+
+        let nextPage = response.headers.get('Link').match(/<(.*?)>; rel='next'/);
+        nextPage = nextPage?.[1];
+        url = nextPage;
+
+        for (let commit of body) yield commit;
+    }
+}
+
+(async () => {
+    let count = 0;
+    for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
+        console.log(commit.author.login);
+        if (++count == 5) break;
+    }
+})();
