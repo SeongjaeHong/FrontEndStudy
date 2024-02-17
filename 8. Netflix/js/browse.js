6d52f87a49c54e2cd11f310d1d3a546d5fc56822
gtw04 <sjhong310@gmail.com>
2024-02-18 00:21:46 +0900

    Add audio and replay button for the main video

---
+++ 8. Netflix/js/browse.js
@@ -0,0 +1,33 @@
+import * as browse_header from './browse_header.js';
+// export {browse_header};
+// export * as browse_header from './browse_header.js';
+import * as browse_body from './browse_body.js';
+import { getSvgPath } from './svg_table.js';
+
+function setSVG() {
+    /*
+    Draw SVGs according to each class name.
+    */
+    const svgEls = document.getElementsByTagName('svg');
+    for (let svgEl of svgEls) {
+        let pathEl = svgEl.getElementsByTagName('path')[0];
+        let value = svgEl.closest('button').attributes['data-uia'].value
+        pathEl.setAttribute('d', getSvgPath(value));
+    }
+}
+
+if (typeof window !== 'undefined') {
+    window.addEventListener('scroll', () => {
+        browse_header.changeBackground();
+    }, true);
+}
+
+// Play main video
+browse_body.content_manager.video = '../files/Bridgerton/Bridgerton_video.mp4';
+browse_body.content_manager.img = '../files/Bridgerton/Bridgerton_stop.webp';
+browse_body.content_manager.runContents();
+// browse_body.runContent(document.body.getElementsByClassName('billboard-presentation ptrack-container')[0],
+//     '../files/Bridgerton/Bridgerton_stop.webp',
+//     '../files/Bridgerton/Bridgerton_video.mp4');
+
+setSVG();
---
+++ 8. Netflix/js/browse_body.js
@@ -0,0 +1,131 @@
+import { getSvgPath } from './svg_table.js';
+
+class contentManager {
+    constructor() {
+        // Set containers.
+        this.ptrack_container = document.body.getElementsByClassName('billboard-presentation ptrack-container')[0];
+        this.embedded_container = document.body.getElementsByClassName('embedded-container')[0];
+        this.meta_container = document.body.getElementsByClassName('meta-container')[0];
+
+        // Set an image and a video element.
+        this.imgEl = document.createElement('img');
+        this.videoEl = document.createElement('video');
+        this._img_path = '';
+        this._video_path = '';
+
+        // Set embedded_button.
+        this.embedded_button = this.embedded_container.getElementsByTagName('button')[0];
+        this.embedded_button_svg_path = this.embedded_button.getElementsByTagName('path')[0];
+        this.embedded_button.setAttribute('data-uia', 'audio-toggle-muted');
+        this.embedded_button_svg_path.setAttribute('d', getSvgPath(this.embedded_button.getAttribute('data-uia')));
+
+        // Setting image attributes.
+        this.imgEl.style.width = '100%';
+
+        // Setting video attributes.
+        this.videoEl.style.width = '100%';
+        
+        // Event map for handling events.
+        this.eventMap = {
+            'audioToggle': this.audioToggle.bind(this),
+            'convertToReplay': this.convertToReplay.bind(this),
+            'convertToAudioToggle': this.convertToAudioToggle.bind(this)
+        }
+
+        // Add default event
+        this.embedded_button.addEventListener('click', this.eventMap['audioToggle']);
+    }
+
+    get img() {
+        return this._img_path;
+    }
+
+    set img(path) {
+        this._img_path = path;
+        this.imgEl.src = this._img_path;
+    }
+
+    get video() {
+        return this._video_path;
+    }
+
+    set video(path) {
+        this._video_path = path;
+        this.videoEl.src = this._video_path;
+    }
+
+    runContents = (content = 'video') => {
+        if (content == 'image') {
+            if (this.ptrack_container.getElementsByTagName('video').length) {
+                this.ptrack_container.removeChild(this.videoEl);
+            }
+            this.ptrack_container.appendChild(this.imgEl);
+
+            // Change audio button to replay button.
+            this.convertToReplay();
+        }
+        else if (content == 'video') {
+            if (this.ptrack_container.getElementsByTagName('img').length) {
+                this.ptrack_container.removeChild(this.imgEl);
+            }
+            this.videoEl.currentTime = 0;
+            this.videoEl.muted = true;
+            this.videoEl.play();
+            this.ptrack_container.appendChild(this.videoEl);
+
+            // A representative image will be displayed after video ends.
+            this.videoEl.addEventListener('ended', () => this.runContents('image'), { once: true });
+        }
+        else {
+            console.log(`Content keyword is not supported: ${content}`)
+        }
+    }
+}
+
+let embeddedContainerFunc = {
+    convertToReplay() {
+        console.log('convertToReplay');
+        // Set SVG to replay.
+        this.embedded_button.setAttribute('data-uia', 'replay');
+        this.embedded_button_svg_path.setAttribute('d', getSvgPath(this.embedded_button.getAttribute('data-uia')));
+
+        // Remove audio function on click.
+        this.embedded_button.removeEventListener('click', this.eventMap['audioToggle']);
+
+        // Add replay function on click.
+        this.embedded_button.addEventListener('click', this.eventMap['convertToAudioToggle']);
+    },
+
+    convertToAudioToggle() {
+        console.log('convertToAudioToggle');
+        // Play video.
+        this.runContents('video');
+
+        // Set SVG to audio (muted)
+        this.embedded_button.setAttribute('data-uia', 'audio-toggle-muted');
+        this.embedded_button_svg_path.setAttribute('d', getSvgPath(this.embedded_button.getAttribute('data-uia')));
+
+        // Remove replay function on click.
+        this.embedded_button.removeEventListener('click', this.eventMap['convertToAudioToggle']);
+
+        // Add audio function on click.        
+        this.embedded_button.addEventListener('click', this.eventMap['audioToggle']);
+    },
+
+    audioToggle() { // Change audio SVG
+        console.log('audioToggle');
+        if (this.embedded_button.getAttribute('data-uia') == 'audio-toggle-unmuted') {
+            this.embedded_button.setAttribute('data-uia', 'audio-toggle-muted');
+            this.videoEl.muted = true;
+
+        }
+        else if (this.embedded_button.getAttribute('data-uia') == 'audio-toggle-muted') {
+            this.embedded_button.setAttribute('data-uia', 'audio-toggle-unmuted');
+            this.videoEl.muted = false;
+        }
+        this.embedded_button_svg_path.setAttribute('d', getSvgPath(this.embedded_button.getAttribute('data-uia')));
+    }
+}
+
+Object.assign(contentManager.prototype, embeddedContainerFunc);
+export const content_manager = new contentManager();
