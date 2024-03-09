bfd8459e00644ec0bd0cbe88125751521792fd4d
Seongjae Hong <sjhong310@gmail.com>
2024-03-10 01:58:37 +0900

    JS - Home opacity

--- Projects/Portfolio/src/main.js
+++ Projects/Portfolio/src/main.js
@@ -1,11 +1,30 @@
-// Apply dark mode when scrolloing.
-const header = document.body.getElementsByClassName('header')[0];
-const headerHeight = header.getBoundingClientRect().height;
-window.addEventListener('scroll', ()=> {
-    if (window.scrollY > headerHeight) {
-        header.classList.add('header--dark');
+class headerManager {
+    constructor() {
+        this.headerEl = document.body.getElementsByClassName('header')[0];
     }
-    else{
-        header.classList.remove('header--dark');
+
+    // Apply dark mode when scrolloing.
+    changeColor(scrollY) {
+        if (scrollY > this.headerEl.offsetHeight) {
+            this.headerEl.classList.add('header--dark');
+        }
+        else {
+            this.headerEl.classList.remove('header--dark');
+        }
     }
+
+    // Open/Close navigation
+    // TODO
+}
+
+const header_manager = new headerManager();
+window.addEventListener('scroll', () => {
+    // Apply dark mode when scrolloing.
+    header_manager.changeColor(window.scrollY);
+});
+
+const home_section = document.getElementById('home');
+window.addEventListener('scroll', () => {
+    // Change opacity of Home section
+    home_section.style.opacity = 1 - window.scrollY / home_section.offsetHeight;
 });
