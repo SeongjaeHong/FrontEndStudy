9366ea3d7afb6fb5cc192d30412648fa11696493
Seongjae Hong <sjhong310@gmail.com>
2024-03-10 04:22:08 +0900

    JS - Menu

--- Projects/Portfolio/src/main.js
+++ Projects/Portfolio/src/main.js
@@ -1,27 +1,23 @@
-class headerManager {
-    constructor() {
-        this.headerEl = document.body.getElementsByClassName('header')[0];
+// Apply dark mode when scrolloing.
+function headerToggle(scrollY) {
+    if (scrollY > header.offsetHeight) {
+        header.classList.add('header--dark');
     }
-
-    // Apply dark mode when scrolloing.
-    changeColor(scrollY) {
-        if (scrollY > this.headerEl.offsetHeight) {
-            this.headerEl.classList.add('header--dark');
-        }
-        else {
-            this.headerEl.classList.remove('header--dark');
-        }
+    else {
+        header.classList.remove('header--dark');
     }
-
-    // Open/Close navigation
-    // TODO
 }
 
-const header_manager = new headerManager();
+// Apply dark mode when scrolloing.
+const header = document.getElementsByClassName('header')[0];
 window.addEventListener('scroll', () => {
-    // Apply dark mode when scrolloing.
-    header_manager.changeColor(window.scrollY);
+    console.log('scroll active');
+    headerToggle(scrollY);
 });
+
+// For the case when the page is refreshed.
+// It must be shown when the scroll is not at the top.
+headerToggle(scrollY);
 
 const home_section = document.getElementById('home');
 window.addEventListener('scroll', () => {
@@ -40,3 +36,15 @@ window.addEventListener('scroll', () => {
     }
     home_section.style.opacity = 1 - window.scrollY / home_section.offsetHeight;
 });
+
+// Open/Close menu tab when the menu-toggle is clicked
+const navbarToggle = document.getElementsByClassName('header__toggle')[0];
+const navbarMenu = document.getElementsByClassName('header__menu')[0];
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Close menu when a menu item is clicked
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
+})
