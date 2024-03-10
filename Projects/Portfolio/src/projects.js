a2f376eea4655d4119c23ea32e94743f7b88c538
Seongjae Hong <sjhong310@gmail.com>
2024-03-11 00:54:42 +0900

    Project fitlering

--- Projects/Portfolio/src/projects.js
+++ Projects/Portfolio/src/projects.js
@@ -1,8 +1,31 @@
 'use strict'
 
+
 // Project filtering
 const categories = document.querySelector('.categories');
-categories.addEventListener('click', (event)=>{
-    console.log(event.target.dataset);
-    console.log(event.target.dataset['category']);
+const projects = document.querySelectorAll('.project');
+categories.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.className == 'category__count') {
+        target = target.parentElement;  // catrygory button
+    }
+
+    const category = target.dataset.category;
+    if (category == null) {
+        return;
+    }
+
+    // Setting active menu
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+
+    projects.forEach(project => {
+        if (project.dataset.type == category || category == 'all') {
+            project.style.display = 'block';
+        }
+        else {
+            project.style.display = 'none';
+        }
+    });
 });
