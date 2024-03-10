abc79a056ce28bf724a3ef0591e69d39a16b481b
Seongjae Hong <sjhong310@gmail.com>
2024-03-11 01:44:08 +0900

    JS - make project filtering functions

--- Projects/Portfolio/src/projects.js
+++ Projects/Portfolio/src/projects.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // Show projects correspond to the clicked category
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
@@ -16,28 +15,29 @@ categories.addEventListener('click', (event) => {
         return;
     }
 
-    // Setting active menu
+    handleActiveSelection(target);
+    filterProjects(category);
+});
+
+function handleActiveSelection(target) {
     const active = document.querySelector('.category--selected');
     active.classList.remove('category--selected');
     target.classList.add('category--selected');
+}
 
-    // Project filtering
+function filterProjects(category) {
     projectsContainer.classList.add('anim-out');
     setTimeout(() => {
-        new Promise(function(resolve) {
-            projectFiltering(category);
+        new Promise(function (resolve) {
+            projects.forEach(project => {
+                if (project.dataset.type == category || category == 'all') {
+                    project.style.display = 'block';
+                }
+                else {
+                    project.style.display = 'none';
+                }
+            });
             resolve;
         }).then(projectsContainer.classList.remove('anim-out'));
     }, 250);  // animation time is 250ms in CSS
-});
-
-function projectFiltering(category){
-    projects.forEach(project => {
-        if (project.dataset.type == category || category == 'all') {
-            project.style.display = 'block';
-        }
-        else {
-            project.style.display = 'none';
-        }
-    });
 }
