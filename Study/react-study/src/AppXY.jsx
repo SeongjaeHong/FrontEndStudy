368a895e5b0db054a36ec38084e6f702421a77b8
Seongjae Hong <sjhong310@gmail.com>
2024-03-16 17:10:42 +0900

    mouse event

---
+++ Study/react-study/src/AppXY.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './AppXY.css'
+
+export default function AppXY() {
+    const [coord, setCoord] = useState({x:0, y:0});
+    const handleMouseMove = (e) => {
+        setCoord({x: e.clientX, y: e.clientY});
+    };
+
+    return (
+        <div className='container' onMouseMove={handleMouseMove}>
+            <div className='pointer' style={
+                {
+                    left: `${coord.x}px`,
+                    top: `${coord.y}px`
+                }
+            }>
+            </div>
+        </div>
+    );
+}
+
