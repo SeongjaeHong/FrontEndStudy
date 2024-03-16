886ab11da17d6a42e2f2e5178d1ea589f30b309b
Seongjae Hong <sjhong310@gmail.com>
2024-03-16 21:38:39 +0900

    Immer

--- Study/react-study/src/index.js
+++ Study/react-study/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
-import AppMentor from './basic/AppMentor';
+import AppMentorImmer from './basic/AppMentorImmer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentor />
+    <AppMentorImmer />
   </React.StrictMode>
 );
 
