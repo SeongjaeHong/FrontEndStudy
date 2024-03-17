134f47cd7b16a1082cb5fc043f1d99cb9f34a57c
Seongjae Hong <sjhong310@gmail.com>
2024-03-17 23:18:23 +0900

    styled component

--- Study/react-study/src/basic/App.js
+++ Study/react-study/src/basic/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid black;
+  color: red;
+`;
 
 export default function App() {
   return (
     <>
       <Button1 />
       <Button2 />
+      <Container>
+        <Button>new button</Button>
+      </Container>
     </>
   );
 }
