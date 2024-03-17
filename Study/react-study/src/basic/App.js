75b62e665b429274f68f55092f8dff2c517a49c5
Seongjae Hong <sjhong310@gmail.com>
2024-03-18 00:23:34 +0900

    test

--- Study/react-study/src/basic/App.js
+++ Study/react-study/src/basic/App.js
@@ -1,27 +1,13 @@
 import "./App.css";
 import Button1 from "./components/Button1";
 import Button2 from "./components/Button2";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-`;
-
-const Button = styled.button`
-  background: transparent;
-  border-radius: 3px;
-  border: 2px solid black;
-  color: red;
-`;
 
 export default function App() {
   return (
     <>
       <Button1 />
       <Button2 />
-      <Container>
-        <Button>new button</Button>
-      </Container>
+      <h1 className="bg-blue-500 rounded-xl px-2 py-2">hi</h1>
     </>
   );
 }
