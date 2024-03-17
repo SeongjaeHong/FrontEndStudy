ce2160545a9071d0a0f1a630e8bdd0f173370d8a
Seongjae Hong <sjhong310@gmail.com>
2024-03-17 23:12:06 +0900

    postcss

---
+++ Study/react-study/src/basic/components/Button1.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import anyname from "./Button1.module.css";
+
+export default function Button1() {
+  return <button className={anyname.button}>Button3</button>;
+}
---
+++ Study/react-study/src/basic/components/Button2.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import anyname from "./Button2.module.css";
+
+export default function Button2() {
+  return <button className={anyname.button}>Button2</button>;
+}
