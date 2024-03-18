b0d493966d6c233a4f37da1ca5ae8794b67135ad
Seongjae Hong <sjhong310@gmail.com>
2024-03-18 22:46:56 +0900

    init

---
+++ Projects/todo/src/components/Todolist.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Todolist() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <input
+            type="checkbox"
+            id="x"
+          />
+          <label for="x">hi</label>
+        </li>
+        <li>
+          <input
+            type="checkbox"
+            id="y"
+          />
+          <label for="x">hi2</label>
+        </li>
+      </ul>
+    </div>
+  );
+}
