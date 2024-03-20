785356700f63c22a79c0e2e49ee8392f17d58080
Seongjae Hong <sjhong310@gmail.com>
2024-03-21 00:27:24 +0900

    fix duplicated input ID in Todo components

--- Projects/todo/src/components/TodoList/Todo.jsx
+++ Projects/todo/src/components/TodoList/Todo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { FaTrashCan } from "react-icons/fa6";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Todo({ todo, onUpdate, onDelete }) {
   const { text, status } = todo;
   const handelChange = (e) => {
@@ -7,15 +9,16 @@ export default function Todo({ todo, onUpdate, onDelete }) {
     onUpdate({ ...todo, status: status });
   };
   const handleDelete = () => onDelete(todo);
+  const chkboxId = uuidv4();
   return (
     <li>
       <input
         type="checkbox"
-        id="checkbox"
+        id={chkboxId}
         checked={status === "completed"}
         onChange={handelChange}
       />
-      <label htmlFor="checkbox">{text}</label>
+      <label htmlFor={chkboxId}>{text}</label>
       <button onClick={handleDelete}>
         <FaTrashCan />
       </button>
