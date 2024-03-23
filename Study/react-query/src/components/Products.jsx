e93ddf73def77cc3a6aaa5234cb5c58115131274
Seongjae Hong <sjhong310@gmail.com>
2024-03-24 01:05:33 +0900

    useQuery & DevTool

---
+++ Study/react-query/src/components/Products.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Products() {
+  const [checked, setChecked] = useState(false);
+  const handleChange = () => setChecked((prev) => !prev);
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products", checked],
+    queryFn: async () => {
+      const res = await fetch(`data/${checked ? "sale_" : ""}products.json`);
+      return res.json();
+    },
+  });
+
+  if (isLoading) {
+    console.log("Loading...");
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+        />
+        Show Only 🔥 Sale
+      </label>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <article>
+              <h3>{product.name}</h3>
+              <p>{product.price}</p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
