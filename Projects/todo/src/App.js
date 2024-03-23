5016535be0d0141f5c3f9b7f50cdd22708ade0af
Seongjae Hong <sjhong310@gmail.com>
2024-03-23 17:15:08 +0900

    LocalStorage

--- Projects/todo/src/App.js
+++ Projects/todo/src/App.js
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Todolist from "./components/TodoList/Todolist";
 import TodoFilter from "./components/header/TodoFilter";
 import { DarkModeProvider } from "./context/DarkModeContext";
+
 const filters = ["all", "active", "completed"];
+const key_localStorageFilter = "todoFilter";
+
 export default function App() {
   const [filter, setFilter] = useState(filters[0]);
+  const handleFilter = (filter) => {
+    setFilter(filter);
+    localStorage.setItem(key_localStorageFilter, filter);
+  };
+
+  useEffect(() => {
+    let localStorageFilter = localStorage.getItem(key_localStorageFilter)
+      ? localStorage.getItem(key_localStorageFilter)
+      : filter;
+    setFilter(localStorageFilter);
+  }, []);
+
   return (
     <DarkModeProvider>
       <TodoFilter
         filters={filters}
         filter={filter}
-        onFilterChange={setFilter}
+        onFilterChange={handleFilter}
       />
       <Todolist filter={filter} />
     </DarkModeProvider>
--- Projects/todo/src/components/TodoList/Todolist.jsx
+++ Projects/todo/src/components/TodoList/Todolist.jsx
@@ -1,31 +1,79 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import AddTodo from "./AddTodo";
 import Todo from "./Todo";
 import styles from "./Todolist.module.css";
 import { DarkModeContext } from "../../context/DarkModeContext";
 
+const key_todoLocalStorage = "todoList";
+
 export default function Todolist({ filter }) {
-  const [todos, setTodos] = useState([
+  const defaultTodos = [
     { id: uuidv4(), text: "shopping", status: "active" },
     { id: uuidv4(), text: "studying", status: "active" },
-  ]);
+  ];
+  const [todos, setTodos] = useState([]);
 
-  const handleUpdate = (updated) =>
+  const handleUpdate = (updated) => {
     setTodos(todos.map((todo) => (todo.id === updated.id ? updated : todo)));
+    updateLocalStorage(updated);
+  };
 
   const handleAdd = (item) => {
     item = item.replace(/\s+/g, "");
     if (item) {
-      setTodos(() => [
-        ...todos,
-        { id: uuidv4(), text: item, status: "active" },
-      ]);
+      item = { id: uuidv4(), text: item, status: "active" };
+      setTodos([...todos, item]);
+      addLocalStorage(item);
     }
   };
 
+  const addLocalStorage = (item, key = key_todoLocalStorage) => {
+    let localItem = JSON.parse(localStorage.getItem(key));
+    if (localItem) {
+      if (item.constructor.name === "Array") {
+        localItem.push(...item);
+      } else {
+        localItem.push(item);
+      }
+
+      localStorage.setItem(key, JSON.stringify(localItem));
+    } else {
+      if (item.constructor.name === "Array") {
+        localStorage.setItem(key, JSON.stringify(item));
+      } else {
+        localStorage.setItem(key, JSON.stringify([item]));
+      }
+    }
+  };
+
+  const updateLocalStorage = (updated, key = key_todoLocalStorage) => {
+    const todos = JSON.parse(localStorage.getItem(key)).map((todo) =>
+      todo.id === updated.id ? updated : todo
+    );
+    localStorage.setItem(key_todoLocalStorage, JSON.stringify(todos));
+  };
+
+  const removeLocalStorage = (item, key = key_todoLocalStorage) => {
+    let todos = JSON.parse(localStorage.getItem(key)).filter(
+      (todo) => todo.id !== item.id
+    );
+    localStorage.setItem(key, JSON.stringify(todos));
+  };
+
+  useEffect(() => {
+    let localItem = JSON.parse(localStorage.getItem(key_todoLocalStorage));
+    if (localItem?.length) {
+      setTodos(todos.concat(localItem));
+    } else {
+      setTodos(defaultTodos);
+      addLocalStorage(defaultTodos);
+    }
+  }, []);
+
   const handleDelete = (deleted) => {
     setTodos(todos.filter((todo) => todo.id !== deleted.id));
+    removeLocalStorage(deleted);
   };
 
   const { darkMode } = useContext(DarkModeContext);
