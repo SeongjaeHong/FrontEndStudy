54db9d611a0989b59e4398bae10e77bcfcc97778
Seongjae Hong <sjhong310@gmail.com>
2024-03-23 17:39:36 +0900

    Remove unnecessary uuid for todo button

--- Projects/todo/src/components/TodoList/Todo.jsx
+++ Projects/todo/src/components/TodoList/Todo.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { FaTrashCan } from "react-icons/fa6";
-import { v4 as uuidv4 } from "uuid";
 import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
-  const { text, status } = todo;
+  const { id, text, status } = todo;
   const handelChange = (e) => {
     const status = e.target.checked ? "completed" : "active";
     onUpdate({ ...todo, status: status });
   };
   const handleDelete = () => onDelete(todo);
-  const chkboxId = uuidv4();
+
   return (
     <li className={styles.todo}>
       <input
         className={styles.checkbox}
         type="checkbox"
-        id={chkboxId}
+        id={id}
         checked={status === "completed"}
         onChange={handelChange}
       />
       <label
         className={styles.text}
-        htmlFor={chkboxId}>
+        htmlFor={id}>
         {text}
       </label>
       <span className={styles.icon}>
