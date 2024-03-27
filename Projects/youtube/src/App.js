e63bbf3624826dac45c81be86a650a1f02677185
Seongjae Hong <sjhong310@gmail.com>
2024-03-27 21:03:20 +0900

    router frame setting

--- Projects/youtube/src/App.js
+++ Projects/youtube/src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
--- Projects/youtube/src/index.js
+++ Projects/youtube/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./page/NotFound";
+import Videos from "./page/Videos";
+import VideoDetail from "./page/VideoDetail";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
 
