59658e7db5692d705f64343ffa70b75afd735af7
Seongjae Hong <sjhong310@gmail.com>
2024-04-06 18:43:08 +0900

    video styling

--- Projects/youtube/src/components/VideoCard.jsx
+++ Projects/youtube/src/components/VideoCard.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { format } from "timeago.js";
 
 export default function VideoCard({ videoKey, video }) {
   const publishTime = video?.publishTime
     ? video.publishTime
     : video.publishedAt;
+
   return (
     <li key={videoKey}>
+      <img
+        className="w-full"
+        src={video.thumbnails.medium.url}
+        alt="thumbnail"
+      />
       <div>
-        <img
-          src={video.thumbnails.default.url}
-          alt="thumbnail"
-        />
-        <h4>{video.title}</h4>
-        <p>{video.channelTitle}</p>
-        <p>{publishTime}</p>
+        <p className="line-clamp-2 font-semibold my-2">{video.title}</p>
+        <p className="text-sm opacity-80">{video.channelTitle}</p>
+        <p className="text-sm opacity-80">{format(publishTime)}</p>
       </div>
     </li>
   );
