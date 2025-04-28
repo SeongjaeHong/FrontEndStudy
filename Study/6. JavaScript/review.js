03209448a800a57c215486b74b33bdecd997fd51
SeongjaeHong <sjhong310@gmail.com>
2025-04-28 14:29:39 +0900

    async/await #2

--- Study/6. JavaScript/review.js
+++ Study/6. JavaScript/review.js
@@ -1,11 +1,37 @@
-async function loadJson(url) {
-  let response = await fetch(url);
-  if (response.status == 200) {
-    let json = response.json(); // (3)
-    return json;
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = "HttpError";
+    this.response = response;
   }
-
-  throw new Error(response.status);
 }
 
-loadJson("no-such-user.json").catch(alert); // Error: 404
+function loadJson(url) {
+  return fetch(url).then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
+  });
+}
+
+// 유효한 사용자를 찾을 때까지 반복해서 username을 물어봄
+async function demoGithubUser() {
+  while (true) {
+    let name = prompt("GitHub username을 입력하세요.", "iliakan");
+    try {
+      let user = await loadJson(`https://api.github.com/users/${name}`);
+      alert(`이름: ${user.name}.`);
+      break;
+    } catch (err) {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("일치하는 사용자가 없습니다. 다시 입력해 주세요.");
+      } else {
+        throw err;
+      }
+    }
+  }
+}
+
+demoGithubUser().catch(alert);
