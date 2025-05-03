3de7d0ea2168520ad9d26b2e1263217340abdd24
seongjaehong <tjdwo0423@gmail.com>
2025-05-04 00:46:39 +0900

    Counter

---
+++ Study/react-review/src/AppCounter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+export default function AppCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='container'>
+      <div className='banner'>
+        Total Count: {count} {count > 10 ? '불' : '얼음'}
+      </div>
+      <div className='counters'>
+        <Counter setTotalCount={setCount} />
+        <Counter setTotalCount={setCount} />
+      </div>
+    </div>
+  );
+}
