afe7961602be0fc0569a82de1581718b243c4567
seongjaehong <tjdwo0423@gmail.com>
2025-05-07 23:18:39 +0900

    move footer into body

--- Projects/todo-re/src/App.jsx
+++ Projects/todo-re/src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import Body from './components/Body';
-import Footer from './components/Footer';
 import Header from './components/header';
 import { ThemeProvider } from './theme/ThemeContext';
-import { todoItems } from './components/TodoItems';
 
 function App() {
-  const [todos, setTodos] = useState(todoItems);
-
   return (
     <ThemeProvider>
       <div id='todoApp'>
         <Header />
-        <Body todos={todos} setTodos={setTodos} />
-        <Footer setTodos={setTodos} />
+        <Body />
       </div>
     </ThemeProvider>
   );
--- Projects/todo-re/src/components/Body.jsx
+++ Projects/todo-re/src/components/Body.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTheme } from '../theme/ThemeContext';
 import { ImBin2 } from 'react-icons/im';
 import './css/body.css';
+import Footer from './Footer';
 
-export default function Body({ todos, setTodos }) {
+export default function Body() {
   const theme = useTheme();
-
+  const [todos, setTodos] = useState(todoItems);
   const handleTodoCheck = (key) =>
     setTodos((prevTodos) =>
       prevTodos.map((todo) =>
@@ -22,28 +23,41 @@ export default function Body({ todos, setTodos }) {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.key !== key));
 
   return (
-    <main className={theme}>
-      {todos.map((todo) => (
-        <div className='job' key={todo.key}>
-          <input
-            type='checkbox'
-            id={'label' + todo.key}
-            onChange={() => handleTodoCheck(todo.key)}
-            checked={todo.status === 'completed'}
-          />
-          <label
-            className={
-              'overflow-hidden text-ellipsis whitespace-nowrap ' + todo.status
-            }
-            htmlFor={'label' + todo.key}
-          >
-            {todo.job}
-          </label>
-          <div className='bin'>
-            <ImBin2 onClick={() => handleDeleteItem(todo.key)} />
+    <>
+      <main className={theme}>
+        {todos.map((todo) => (
+          <div className='job' key={todo.key}>
+            <input
+              type='checkbox'
+              id={'label' + todo.key}
+              onChange={() => handleTodoCheck(todo.key)}
+              checked={todo.status === 'completed'}
+            />
+            <label
+              className={
+                'overflow-hidden text-ellipsis whitespace-nowrap ' + todo.status
+              }
+              htmlFor={'label' + todo.key}
+            >
+              {todo.job}
+            </label>
+            <div className='bin'>
+              <ImBin2 onClick={() => handleDeleteItem(todo.key)} />
+            </div>
           </div>
-        </div>
-      ))}
-    </main>
+        ))}
+      </main>
+      <Footer setTodos={setTodos} />
+    </>
   );
 }
+
+const todoItems = [
+  { key: 1, job: 'job1', status: 'active' },
+  {
+    key: 2,
+    job: 'job2 which is very long name so it can very likely not be displayed in one line, so it should be managed somehow',
+    status: 'completed',
+  },
+  { key: 3, job: 'job3', status: 'active' },
+];
