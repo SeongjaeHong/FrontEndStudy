503304f34f0f58fd39185ce185ff50e43e0cd44c
SeongjaeHong <sjhong310@gmail.com>
2025-05-15 17:34:42 +0900

    Remove unused css file

--- Projects/youtube_simple_re/src/App.jsx
+++ Projects/youtube_simple_re/src/App.jsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.css';
 import SearchHeader from './components/searchHeader';
 import { Outlet } from 'react-router';
 import { YoutubeApiProvider } from './context/YoutubeApiContext';
