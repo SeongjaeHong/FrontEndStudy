da4ecc32fc2908f9b7196fd662efc0bf5e0f2d12
SeongjaeHong <sjhong310@gmail.com>
2025-05-17 01:24:53 +0900

    init checkbox func

--- Projects/shoppy/src/pages/Edit.jsx
+++ Projects/shoppy/src/pages/Edit.jsx
@@ -1,39 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import './css/Edit.css';
 
 export default function Edit() {
+  const checkboxCount = 2; // TODO: 한 개 페이지에서 보여줄 아이템 항목 갯수로 사용 (가변)
+  const [checkedBoxes, setCheckedBoxes] = useState(
+    new Array(checkboxCount).fill(false)
+  );
+  const checkboxChangeHandle = (index) => {
+    const updatedBoxes = [...checkedBoxes];
+    updatedBoxes[index] = !updatedBoxes[index];
+    setCheckedBoxes(updatedBoxes);
+  };
+
+  const mockItemList = [
+    ['바지', '새바지', '남성', 12300],
+    ['장갑', '벙어리 장갑', '여성', 9380],
+  ];
+
+  const isAnyChecked = checkedBoxes.some(Boolean);
+
   return (
-    <section className='item-list'>
-      <div className='tab'>
-        <span className='col-span-2'>
-          <input type='checkbox' />
-        </span>
-        <span className='col-span-4'>번호</span>
-        <span className='col-span-10'>카테고리</span>
-        <span className='col-span-70'>상품명</span>
-        <span className='col-span-4'>성별</span>
-        <span className='col-span-10'>가격</span>
-      </div>
-      <div className='item'>
-        <span className='col-span-2'>
-          <input type='checkbox' />
-        </span>
-        <span className='col-span-4'>1</span>
-        <span className='col-span-10'>바지</span>
-        <span className='col-span-70'>새바지</span>
-        <span className='col-span-4'>남성</span>
-        <span className='col-span-10'>12300</span>
-      </div>
-      <div className='item'>
-        <span className='col-span-2'>
-          <input type='checkbox' />
-        </span>
-        <span className='col-span-4'>2</span>
-        <span className='col-span-10'>장갑</span>
-        <span className='col-span-70'>벙어리 장갑</span>
-        <span className='col-span-4'>여성</span>
-        <span className='col-span-10'>9380</span>
-      </div>
-    </section>
+    <>
+      <span>Check?:{isAnyChecked ? 'true' : 'false'}</span>
+      <section className='item-list'>
+        <div className='tab'>
+          <span className='col-span-2'></span>
+          <span className='col-span-4'>번호</span>
+          <span className='col-span-10'>카테고리</span>
+          <span className='col-span-70'>상품명</span>
+          <span className='col-span-4'>성별</span>
+          <span className='col-span-10'>가격</span>
+        </div>
+        {mockItemList.map((mockItem, index) => (
+          <div className='item' key={index}>
+            <span className='col-span-2'>
+              <input
+                type='checkbox'
+                className='checkbox'
+                onClick={() => checkboxChangeHandle(index)}
+              />
+            </span>
+            <span className='col-span-4'>{index + 1}</span>
+            <span className='col-span-10'>{mockItem[0]}</span>
+            <span className='col-span-70'>{mockItem[1]}</span>
+            <span className='col-span-4'>{mockItem[2]}</span>
+            <span className='col-span-10'>{mockItem[3]}</span>
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
