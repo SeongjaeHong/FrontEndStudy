ca35fcf8a79a08cc062673798f71dfdc1f6585b6
SeongjaeHong <sjhong310@gmail.com>
2025-05-16 15:19:54 +0900

    Init routes

--- Projects/shoppy/src/App.jsx
+++ Projects/shoppy/src/App.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import { Outlet } from 'react-router';
+import Header from './components/Header';
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
--- Projects/shoppy/src/main.jsx
+++ Projects/shoppy/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import NotFound from './pages/NotFound';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    children: [{ index: true, Component: Home }],
+  },
+  { path: '*', Component: NotFound },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
