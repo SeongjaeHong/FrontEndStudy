3d05562730ada09e5d2a21817486d7992f222ba7
SeongjaeHong <sjhong310@gmail.com>
2025-05-17 01:28:33 +0900

    Init home main image

--- Projects/shoppy/src/pages/Home.jsx
+++ Projects/shoppy/src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { useDB } from '../App';
+import ItemCard from '../components/ItemCard';
+import './css/Home.css';
 
 export default function Home() {
+  const db = useDB();
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div className='main-image'>
+        <img src='/image/main_image_wide.webp' alt='main image' />
+      </div>
+      <section className='items'>
+        {/* <ItemCard></ItemCard> */}
+        {/* getItems(db); */}
+        <span>테스트 텍스트</span>
+      </section>
+    </>
   );
+}
+
+async function getItems(db) {
+  const itemsCol = collection(db, 'items');
+  const itemSnapshot = await getDocs(itemsCol);
+  const itemList = itemSnapshot.docs.map((doc) => doc.data());
+  return itemList;
 }
