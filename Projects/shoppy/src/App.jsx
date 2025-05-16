9196118bb27fced57f7afbfe5f1d8c07eedf03bf
SeongjaeHong <sjhong310@gmail.com>
2025-05-17 01:29:15 +0900

    add firebase db provider

--- Projects/shoppy/src/App.jsx
+++ Projects/shoppy/src/App.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from 'react-router';
 import Header from './components/Header';
+import { createContext, useContext } from 'react';
+import { getFirestore } from 'firebase/firestore/lite';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../firebase.config';
+
+const dbContext = createContext();
+const db = getFirestore(initializeApp(firebaseConfig));
 
 export default function App() {
   return (
-    <>
+    <dbContext.Provider value={db}>
       <Header />
       <Outlet />
-    </>
+    </dbContext.Provider>
   );
+}
+
+export function useDB() {
+  return useContext(dbContext);
 }
