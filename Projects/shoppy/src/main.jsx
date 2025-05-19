0c98576f239885c90b1714f208b5d55e1f348485
SeongjaeHong <sjhong310@gmail.com>
2025-05-19 15:42:46 +0900

    add error element

--- Projects/shoppy/src/main.jsx
+++ Projects/shoppy/src/main.jsx
@@ -1,19 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError,
+} from 'react-router';
 import NotFound from './pages/NotFound';
 import App from './App.jsx';
 import Home from './pages/Home.jsx';
 import './index.css';
 import Edit from './pages/Edit.jsx';
+import ItemDetail from './pages/ItemDetail.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     Component: App,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, Component: Home },
       { path: 'edit', Component: Edit },
+      { path: 'detail', Component: ItemDetail },
     ],
   },
   { path: '*', Component: NotFound },
@@ -24,3 +31,8 @@ createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </StrictMode>
 );
+
+function ErrorPage() {
+  const error = useRouteError();
+  return <div style={{ color: 'red' }}>에러 발생: {error.message}</div>;
+}
