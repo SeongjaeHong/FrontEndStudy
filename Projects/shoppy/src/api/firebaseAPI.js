0ce69e2e35c055459306fb54b882012d9ca2bdfa
SeongjaeHong <sjhong310@gmail.com>
2025-05-21 19:51:26 +0900

    separate firebase api

--- Projects/shoppy/src/api/firebaseAPI.js
+++ Projects/shoppy/src/api/firebaseAPI.js
@@ -1,7 +1,21 @@
 import { get, push, ref, remove, update } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from '@firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBAaI3cnyt2xWB_yh34Frt2JC3B8Q2ROXY',
+  authDomain: 'shoppy-e822e.firebaseapp.com',
+  databaseURL:
+    'https://shoppy-e822e-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'shoppy-e822e',
+  storageBucket: 'shoppy-e822e.firebasestorage.app',
+  messagingSenderId: '160873879655',
+  appId: '1:160873879655:web:54440e3e8cfce779fe9cc2',
+  measurementId: 'G-R9RRX5S9HX',
+};
+export const firebaseDB = getDatabase(initializeApp(firebaseConfig));
 
 const ITEM_PATH = 'items';
-
 export async function saveItem(request) {
   const formData = request.formData;
   const db = request.db;
