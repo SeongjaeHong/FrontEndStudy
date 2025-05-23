372ae3e11446a7faadca43bf8c1b77f66505c085
SeongjaeHong <sjhong310@gmail.com>
2025-05-23 19:09:22 +0900

    deal with the case when id is given

--- Projects/shoppy/src/api/firebaseAPI.js
+++ Projects/shoppy/src/api/firebaseAPI.js
@@ -9,15 +9,14 @@ import {
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBAaI3cnyt2xWB_yh34Frt2JC3B8Q2ROXY',
-  authDomain: 'shoppy-e822e.firebaseapp.com',
-  databaseURL:
-    'https://shoppy-e822e-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'shoppy-e822e',
-  storageBucket: 'shoppy-e822e.firebasestorage.app',
-  messagingSenderId: '160873879655',
-  appId: '1:160873879655:web:156664c6855a5e41fe9cc2',
-  measurementId: 'G-ZFL94KV6C8',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 const app = initializeApp(firebaseConfig);
 export const firebaseDB = getDatabase(app);
@@ -46,10 +45,18 @@ export async function removeItem(removeIds) {
   update(dbRef(firebaseDB), removePaths);
 }
 
-export async function fetchItem() {
-  const snapshot = await get(dbRef(firebaseDB, ITEM_PATH));
+export async function fetchItem(id) {
+  let itemPath = ITEM_PATH;
+  if (id) {
+    itemPath += `/${id}`;
+  }
+  const snapshot = await get(dbRef(firebaseDB, itemPath));
   if (snapshot.exists()) {
     const data = snapshot.val();
+    if (id) {
+      return data;
+    }
+
     const items = Object.entries(data).map(([id, value]) => ({
       id,
       ...value,
