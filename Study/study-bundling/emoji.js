af373158a2705d7ce2d1c892f49c75ef8f0efdcb
SeongjaeHong <sjhong310@gmail.com>
2025-07-07 16:41:27 +0900

    use ecma module

--- Study/study-bundling/emoji.js
+++ Study/study-bundling/emoji.js
@@ -1,8 +1,8 @@
-const emojis = [
+export const emojis = [
   { icon: '😊', name: 'Smiling Face' },
   { icon: '🚀', name: 'Rocket' },
   { icon: '🍕', name: 'Pizza' },
   { icon: '🐱', name: 'Cat' },
   { icon: '🌈', name: 'Rainbow' },
-  { icon: '🎸', name: 'Guitar' }
+  { icon: '🎸', name: 'Guitar' },
 ];
